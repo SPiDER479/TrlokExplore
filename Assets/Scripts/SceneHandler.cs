0c9b0ed7b2539e8c837f709c7da13b00326d77a0
Vaibhav Agarwal <agarwalvaibhav200@gmail.com>
2026-01-29 05:13:40 +0530

    Music

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -4,16 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
+    public static SceneHandler Instance;
+    
     private void Awake()
     {
+        if (!Instance) Instance = this;
+        else Destroy(gameObject);
+        
         DontDestroyOnLoad(gameObject);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P)) SceneManager.LoadSceneAsync("Solar System");
-        if (Input.GetKeyDown(KeyCode.O)) SceneManager.LoadSceneAsync("Jupiter");
-        if (Input.GetKeyDown(KeyCode.I)) SceneManager.LoadSceneAsync("Custom");
-        if (Input.GetKeyDown(KeyCode.U)) SceneManager.LoadSceneAsync("AI");
+        if (Input.GetKeyDown(KeyCode.Alpha0)) SceneManager.LoadSceneAsync("Solar System");
+        if (Input.GetKeyDown(KeyCode.Alpha9)) SceneManager.LoadSceneAsync("Jupiter");
+        if (Input.GetKeyDown(KeyCode.Alpha8)) SceneManager.LoadSceneAsync("Custom");
+        if (Input.GetKeyDown(KeyCode.Alpha7)) SceneManager.LoadSceneAsync("AI");
     }
 }
