404ab45dda77a4b6d778764a6e41a555da4f8bb6
Vaibhav Agarwal <agarwalvaibhav200@gmail.com>
2026-01-29 03:13:35 +0530

    Custom Generator

--- Assets/Scenes/Custom/PlanetCustomiser.cs
+++ Assets/Scenes/Custom/PlanetCustomiser.cs
@@ -1,4 +1,3 @@
-using System;
 using SpaceGraphicsToolkit.Cloud;
 using SpaceGraphicsToolkit.Landscape;
 using SpaceGraphicsToolkit.Ocean;
@@ -7,17 +6,52 @@ using UnityEngine;
 
 public class PlanetCustomiser : MonoBehaviour
 {
-    [SerializeField] private SgtSphereLandscape landscape;
+    [SerializeField] private SgtLandscapeBundle landscape;
     [SerializeField] private SgtSky sky;
     [SerializeField] private SgtCloud cloud;
     [SerializeField] private SgtOcean ocean;
     [SerializeField] private SgtOceanRays oceanRays;
     [SerializeField] private SgtOceanDebris oceanDebris;
 
-    [SerializeField] [Range(0f, 2f)] private float wavesDisplacement;
-
-    private void Update()
+    [SerializeField] private Texture2D[] heightmaps;
+    public void RandomizeHeight()
     {
-        ocean.WavesDisplacement = wavesDisplacement;
+        landscape.HeightTextures[0] = heightmaps[Random.Range(0, heightmaps.Length)];
+        
+        landscape.MarkAsDirty();
+        landscape.Regenerate();
+    }
+    
+    [SerializeField] private Texture2D[] gradients;
+    public void RandomizeColor()
+    {
+        landscape.GradientTextures[0] = gradients[Random.Range(0, gradients.Length)];
+        
+        landscape.MarkAsDirty();
+        landscape.Regenerate();
+    }
+    
+    public float RaleighColorR { set { raleighColorR = value; } get { return raleighColorR; } } [SerializeField] private float raleighColorR = 0.09f;
+    public float RaleighColorG { set { raleighColorG = value; } get { return raleighColorG; } } [SerializeField] private float raleighColorG = 0.24f;
+    public float RaleighColorB { set { raleighColorB = value; } get { return raleighColorB; } } [SerializeField] private float raleighColorB = 1f;
+    public void SkyColor()
+    {
+        sky.RayleighColor = new Color(raleighColorR, raleighColorG, raleighColorB, 1);
+    }
+    
+    public float CloudColorR { set { cloudColorR = value; } get { return cloudColorR; } } [SerializeField] private float cloudColorR = 1f;
+    public float CloudColorG { set { cloudColorG = value; } get { return cloudColorG; } } [SerializeField] private float cloudColorG = 0.91f;
+    public float CloudColorB { set { cloudColorB = value; } get { return cloudColorB; } } [SerializeField] private float cloudColorB = 0.5f;
+    public void CloudColor()
+    {
+        cloud.Color = new Color(cloudColorR, cloudColorG, cloudColorB, 1);
+    }
+    
+    public float OceanColorR { set { oceanColorR = value; } get { return oceanColorR; } } [SerializeField] private float oceanColorR = 0f;
+    public float OceanColorG { set { oceanColorG = value; } get { return oceanColorG; } } [SerializeField] private float oceanColorG = 0.36f;
+    public float OceanColorB { set { oceanColorB = value; } get { return oceanColorB; } } [SerializeField] private float oceanColorB = 0.5f;
+    public void OceanColor()
+    {
+        ocean.SurfaceColor = new Color(oceanColorR, oceanColorG, oceanColorB, 1);
     }
 }
