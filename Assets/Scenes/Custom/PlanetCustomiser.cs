3e8465454fa93c8ff0b7b5335274cb48652deac4
Vaibhav Agarwal <agarwalvaibhav200@gmail.com>
2026-02-05 02:10:33 +0530

    Current State

--- Assets/Scenes/Custom/PlanetCustomiser.cs
+++ Assets/Scenes/Custom/PlanetCustomiser.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections;
 using SpaceGraphicsToolkit.Cloud;
 using SpaceGraphicsToolkit.Landscape;
 using SpaceGraphicsToolkit.Ocean;
 using SpaceGraphicsToolkit.Sky;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlanetCustomiser : MonoBehaviour
 {
@@ -30,28 +33,60 @@ public class PlanetCustomiser : MonoBehaviour
         landscape.MarkAsDirty();
         landscape.Regenerate();
     }
-    
-    public float RaleighColorR { set { raleighColorR = value; } get { return raleighColorR; } } [SerializeField] private float raleighColorR = 0.09f;
-    public float RaleighColorG { set { raleighColorG = value; } get { return raleighColorG; } } [SerializeField] private float raleighColorG = 0.24f;
-    public float RaleighColorB { set { raleighColorB = value; } get { return raleighColorB; } } [SerializeField] private float raleighColorB = 1f;
-    public void SkyColor()
+
+    public float Nitrogen { set { nitrogen = value; } get { return nitrogen; } } [SerializeField] private float nitrogen = 0.001f;
+    public float Oxygen { set { oxygen = value; } get { return oxygen; } } [SerializeField] private float oxygen = 0.001f;
+    public float CarbonDioxide { set { carbonDioxide = value; } get { return carbonDioxide; } } [SerializeField] private float carbonDioxide = 0.001f;
+    public float Methane { set { methane = value; } get { return methane; } } [SerializeField] private float methane = 0.001f;
+    public float SulfurDioxide { set { sulfurDioxide = value; } get { return sulfurDioxide; } } [SerializeField] private float sulfurDioxide = 0.001f;
+    public void CalculatePlanet()
     {
-        sky.RayleighColor = new Color(raleighColorR, raleighColorG, raleighColorB, 1);
+        CalculateAtmosphere();
+        CalculateCloud();
+        CalculateWater();
     }
-    
-    public float CloudColorR { set { cloudColorR = value; } get { return cloudColorR; } } [SerializeField] private float cloudColorR = 1f;
-    public float CloudColorG { set { cloudColorG = value; } get { return cloudColorG; } } [SerializeField] private float cloudColorG = 0.91f;
-    public float CloudColorB { set { cloudColorB = value; } get { return cloudColorB; } } [SerializeField] private float cloudColorB = 0.5f;
-    public void CloudColor()
+    private void CalculateAtmosphere()
     {
-        cloud.Color = new Color(cloudColorR, cloudColorG, cloudColorB, 1);
+        float total = nitrogen + oxygen + carbonDioxide + methane + sulfurDioxide;
+
+        float n2 = nitrogen / total;
+        float o2 = oxygen / total;
+        float co2 = carbonDioxide / total;
+        float ch4 = methane / total;
+        float so2 = sulfurDioxide / total;
+
+        float r = 0.3f * n2 + 0.4f * o2 + 1f * co2 + 0f * ch4 + 1f * so2;
+        float g = 0.5f * n2 + 0.6f * o2 + 0.6f * co2 + 0.8f * ch4 + 1f * so2;
+        float b = 1f * n2 + 1f * o2 + 0.6f * co2 + 1f * ch4 + 0.2f * so2;
+        
+        float M = n2 * 28.02f + o2 * 32.00f + co2 * 44.01f + ch4 * 16.04f + so2 * 64.07f;
+        float relativeDensity = (M - 16f) / (64f - 16f);
+        
+        sky.RayleighColor = new Color(Mathf.Clamp01(r), Mathf.Clamp01(g), Mathf.Clamp01(b), 1f);
+        sky.Density = Mathf.Clamp01(relativeDensity);
     }
-    
-    public float OceanColorR { set { oceanColorR = value; } get { return oceanColorR; } } [SerializeField] private float oceanColorR = 0f;
-    public float OceanColorG { set { oceanColorG = value; } get { return oceanColorG; } } [SerializeField] private float oceanColorG = 0.36f;
-    public float OceanColorB { set { oceanColorB = value; } get { return oceanColorB; } } [SerializeField] private float oceanColorB = 0.5f;
-    public void OceanColor()
+    private void CalculateCloud()
     {
-        ocean.SurfaceColor = new Color(oceanColorR, oceanColorG, oceanColorB, 1);
+        float cloudWhiteness = Mathf.Clamp01((carbonDioxide + sulfurDioxide) / 200f);
+
+        cloud.Color = Color.Lerp(sky.RayleighColor, Color.white, cloudWhiteness);
+    }
+    private void CalculateWater()
+    {
+        Color deepBlue = new Color(0.0f, 0.2f, 0.35f);
+
+        float darkening = Mathf.Clamp01(methane / 150f);
+        float muddy = Mathf.Clamp01(sulfurDioxide / 200f);
+
+        Color deepColor = Color.Lerp(deepBlue, Color.black, darkening);
+        deepColor = Color.Lerp(deepColor, new Color(0.3f, 0.3f, 0.2f), muddy);
+        deepColor = new Color(Mathf.Clamp01(deepColor.r), Mathf.Clamp01(deepColor.g), Mathf.Clamp01(deepColor.b), 1f);
+
+        Color reflectedSky = sky.RayleighColor * 0.7f;
+        Color surfaceColor = Color.Lerp(reflectedSky + deepColor * 0.3f, deepColor, 0.1f);
+        surfaceColor = new Color(Mathf.Clamp01(surfaceColor.r), Mathf.Clamp01(surfaceColor.g), Mathf.Clamp01(surfaceColor.b), 1f);
+        
+        ocean.SurfaceColor = surfaceColor;
+        ocean.SurfaceScattering = deepColor;
     }
 }
